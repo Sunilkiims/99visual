0ce7876fb4cf689b7c6b0d9a07948953f2cbeaf1
Sunil Behera <sunilkiims@gmail.com>
2026-06-15 10:54:20 +0530

    fix: prisma

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,19 +1,6 @@
 import path from 'node:path'
 import { defineConfig } from 'prisma/config'
-import { PrismaPg } from '@prisma/adapter-pg'
-import pg from 'pg'
-
-const { Pool } = pg
 
 export default defineConfig({
   schema: path.join('prisma', 'schema.prisma'),
-  migrate: {
-    async adapter() {
-      const pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-      })
-
-      return new PrismaPg(pool)
-    },
-  },
 })
