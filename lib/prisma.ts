8b5f0b8f1892e11373efdcc374deb33a36c05ab5
Sunil Behera <sunilkiims@gmail.com>
2026-06-15 10:41:36 +0530

    fix: use require for PrismaClient compatibility with Prisma 7

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,21 @@
-import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import pg from 'pg'
 
 const { Pool } = pg
 
 const connectionString = process.env.DATABASE_URL!
-
 const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { PrismaClient } = require('@prisma/client')
+
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma: any
 }
 
-export const prisma =
+export const prisma: any =
   globalForPrisma.prisma ??
-  new PrismaClient({ adapter } as any)
+  new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
