5451c36722c695c2e708650476d37fb0341d7911
Sunilkiims <49035308+Sunilkiims@users.noreply.github.com>
2026-07-25 22:48:56 +0530

    Update page.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -12,8 +12,7 @@ import Link from "next/link";
 import Header         from "@/app/components/header";
 import Footer         from "@/app/components/footer";
 import ScrollDown     from "@/app/components/scrolldown";
-import Chatbot        from "@/app/components/chatbot";
-import Whatsappbutton from "@/app/components/wahtsappbutton";
+
 
 
 import {
@@ -650,8 +649,7 @@ export default function ServicesPage() {
 
       <Footer />
       <ScrollDown />
-      <Chatbot />
-      <Whatsappbutton />
+      
     </>
   );
 }
